2def44895b41973abe283877fcdfea8dae32af0d
DonggeonMoon <nullsafer@naver.com>
2022-06-19 00:28:06 +0900

    write ValidationUtils draft

---
+++ src/ValidationUtils/Validation.java
@@ -0,0 +1,51 @@
+package ValidationUtils;
+
+public class Validation {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isNotNull(String input) {
+        return false;
+    }
+
+    public boolean isNotBlank(String input) {
+        return false;
+    }
+
+    public boolean isNotEmpty(String input) {
+        return false;
+    }
+
+    public boolean isNumberOnly(String input) {
+        return false;
+    }
+
+    public boolean isAlphabetOnly(String input) {
+        return false;
+    }
+
+    public boolean isHangeulOnly(String input) {
+        return false;
+    }
+
+    public boolean isEmail(String input) {
+        return false;
+    }
+
+    public boolean isMobilePhoneNumber(String input) {
+        return false;
+    }
+
+    public boolean hasCorrectDateFormat(String input) {
+        return false;
+    }
+
+    public String stripCharacters(String input) {
+        return null;
+    }
+
+    public String stripNumbers(String input) {
+        return null;
+    }
+}
