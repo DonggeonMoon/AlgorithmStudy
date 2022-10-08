84296dfc982382f526759786c54c15e9b4c5886e
DonggeonMoon <nullsafer@naver.com>
2022-10-08 23:32:12 +0900

    reflection 테스트
    자식 클래스에서 부모 클래스의 필드를 가져올 수 있는지 테스트

--- test/reflection/ChildTest.java
+++ test/reflection/ChildTest.java
@@ -12,7 +12,6 @@ public class ChildTest {
 
         for (Field field : cls.getSuperclass().getDeclaredFields()) {
             System.out.println("field.getName() = " + field.getName());
-            field.
         }
 
         for (Field field : cls.getDeclaredFields()) {
