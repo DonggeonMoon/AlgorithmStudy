c811b08ea07f6cadc5a7f9293b4d8788875c7c57
DonggeonMoon <nullsafer@naver.com>
2022-11-12 20:52:30 +0900

    feat: java 9 이후 추가된 readAllBytes()로 바이트 배열 바로 가져오기

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,7 @@ public class Main {
         File file = new File("test.txt");
         FileInputStream fileInputStream = new FileInputStream(file);
         BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-        byte[] bytes = new byte[100];
-
-        for(int i = 0; i < bytes.length; i++) {
-            bytes[i] = (byte) bufferedInputStream.read();
-        }
+        byte[] bytes = bufferedInputStream.readAllBytes();
         System.out.println(new String(bytes, StandardCharsets.UTF_8));
-
-
     }
 }
