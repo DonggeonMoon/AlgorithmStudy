01c495f6d2a234bff311f25b060ac1d6242e1f09
DonggeonMoon <nullsafer@naver.com>
2023-11-11 23:49:06 +0900

    chore: 피보나치 문제 이동

--- Q1003/src/Main.java
+++ Q1003/src/Main.java
@@ -1,8 +1,6 @@
-package practice;
-
 import java.util.Scanner;
 
-public class Q1004 {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
