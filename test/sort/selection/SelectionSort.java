110f5e1f4d64e95f2343a125be3f29fa580d5635
DonggeonMoon <nullsafer@naver.com>
2023-11-17 12:05:46 +0900

    feat: 삽입 정렬

---
+++ test/sort/selection/SelectionSort.java
@@ -0,0 +1,21 @@
+package sort.selection;
+
+import sort.Sort;
+
+public class SelectionSort extends Sort {
+    @Override
+    public void sort(int[] inputNumbers) {
+        for (int i = 0; i < inputNumbers.length - 1; i++) {
+            int minimumIndex = i;
+            for (int j = i + 1; j < inputNumbers.length; j++) {
+                if (inputNumbers[j] < inputNumbers[minimumIndex]) {
+                    minimumIndex = j;
+                    swap(inputNumbers, i, minimumIndex);
+                }
+            }
+            if (minimumIndex != i) {
+                swap(inputNumbers, i, minimumIndex);
+            }
+        }
+    }
+}
