72df481cc3e41380aea122f9dcb1c835ad8d23d5
DonggeonMoon <nullsafer@naver.com>
2023-11-18 01:18:24 +0900

    refactor: 정적 팩토리 메서드 사용

--- test/search/sequential/SequentialSearch.java
+++ test/search/sequential/SequentialSearch.java
@@ -3,6 +3,13 @@ package search.sequential;
 import java.util.NoSuchElementException;
 
 public class SequentialSearch {
+    private SequentialSearch() {
+    }
+
+    public static SequentialSearch create() {
+        return new SequentialSearch();
+    }
+
     public int search(int target, int[] numbers) {
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == target) {
