8e0782a14b73ba6f5d1c5f2fc6c9e4fc94417dc2
DonggeonMoon <nullsafer@naver.com>
2023-11-17 12:05:32 +0900

    feat: 병합 정렬

---
+++ test/sort/merge/MergeSort.java
@@ -0,0 +1,52 @@
+package sort.merge;
+
+import sort.Sort;
+
+import java.util.Arrays;
+
+public class MergeSort extends Sort {
+
+    @Override
+    public void sort(final int[] numbers) {
+        int length = numbers.length;
+        if (length <= 1) {
+            return;
+        }
+
+        int mid = length / 2;
+
+        int[] group1 = Arrays.copyOfRange(numbers, 0, mid);
+        int[] group2 = Arrays.copyOfRange(numbers, mid, length);
+
+        sort(group1);
+        sort(group2);
+
+        int groupIndex1 = 0;
+        int groupIndex2 = 0;
+        int numberIndex = 0;
+
+        while (groupIndex1 < group1.length && groupIndex2 < group2.length) {
+            if (group1[groupIndex1] < group2[groupIndex2]) {
+                numbers[numberIndex] = group1[groupIndex1];
+                groupIndex1++;
+                numberIndex++;
+                continue;
+            }
+            numbers[numberIndex] = group2[groupIndex2];
+            groupIndex2++;
+            numberIndex++;
+        }
+
+        while (groupIndex1 < group1.length) {
+            numbers[numberIndex] = group1[groupIndex1];
+            groupIndex1++;
+            numberIndex++;
+        }
+
+        while (groupIndex2 < group2.length) {
+            numbers[numberIndex] = group2[groupIndex2];
+            groupIndex2++;
+            numberIndex++;
+        }
+    }
+}
