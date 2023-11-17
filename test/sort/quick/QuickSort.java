017e3b340526a33f8f7b6224db46c29ad79cfff2
DonggeonMoon <nullsafer@naver.com>
2023-11-17 12:05:38 +0900

    feat: 퀵 정렬

---
+++ test/sort/quick/QuickSort.java
@@ -0,0 +1,31 @@
+package sort.quick;
+
+import sort.Sort;
+
+public abstract class QuickSort extends Sort {
+    @Override
+    protected void sort(final int[] numbers) {
+        subSort(numbers, 0, numbers.length - 1);
+    }
+
+    private void subSort(int[] numbers, int start, int end) {
+        if (end - start <= 0) {
+            return;
+        }
+
+        int pivot = numbers[end];
+        int i = start;
+
+        for (int j = start; j < end; j++) {
+            if (numbers[j] <= pivot) {
+                swap(numbers, i, j);
+                i++;
+            }
+
+            swap(numbers, i, end);
+        }
+        subSort(numbers, start, i - 1);
+        subSort(numbers, i + 1, end);
+    }
+
+}
